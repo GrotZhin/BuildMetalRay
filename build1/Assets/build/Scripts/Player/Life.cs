ae4ff77247b8c0fa8a3d03f41ed19636d3282c35
Reg-Meme <michaeljacksonnothenoite@gmail.com>
2024-11-23 22:17:38 -0300

    ver 2.0.3
    healing item visual effects

--- build1/Assets/build/Scripts/Player/Life.cs
+++ build1/Assets/build/Scripts/Player/Life.cs
@@ -7,12 +7,16 @@ namespace MetalRay
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+    public ParticleSystem vfxhit;
+     public ParticleSystem vfxhit2;
      public int restaure = 40;
      void OnTriggerEnter(Collider hitInfo){
            PlayerLife playerLife = hitInfo.GetComponent<PlayerLife>();
            if(playerLife != null){
                 playerLife.RestaureLife(restaure);
                 Destroy(gameObject);
+                Instantiate(vfxhit, transform.position, Quaternion.identity);
+                Instantiate(vfxhit2, transform.position, Quaternion.identity);
            }
     }
 }

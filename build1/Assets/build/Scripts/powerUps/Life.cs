7154b40b06a8fcdbeb52d40645c559afd1927a60
GrotZhin <gabrielcamposlol20@gmail.com>
2024-11-26 23:50:36 -0300

    ver2.0.5
    i dont know, help me pls God

---
+++ build1/Assets/build/Scripts/powerUps/Life.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MetalRay
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+    public ParticleSystem vfx;
+     public ParticleSystem vfx2;
+     public int restaure = 40;
+     void OnTriggerEnter(Collider hitInfo){
+           PlayerLife playerLife = hitInfo.GetComponent<PlayerLife>();
+           if(playerLife != null){
+                playerLife.RestaureLife(restaure);
+                Destroy(gameObject);
+                Instantiate(vfx, transform.position, Quaternion.identity);
+                Instantiate(vfx2, transform.position, Quaternion.identity);
+           }
+    }
+}
+}
--- build1/Assets/build/Scripts/powerUps/ReverbPowerUp.cs
+++ build1/Assets/build/Scripts/powerUps/ReverbPowerUp.cs
@@ -9,7 +9,12 @@ namespace MetalRay
     {
         public static bool active;
         public GameObject model;
-        float time;
+
+         public ParticleSystem vfx;
+         public ParticleSystem vfx2;
+         public ParticleSystem vfx3;
+         float time;
+
         void OnTriggerEnter(Collider other)
         {
             active = true;
@@ -24,17 +29,20 @@ namespace MetalRay
             {
                 enemyShoot.speed = 1.5f;
                 Destroy(model);
+                Instantiate(vfx, transform.position, Quaternion.identity);
+                Instantiate(vfx2, transform.position, Quaternion.identity);
+                Instantiate(vfx3, transform.position, Quaternion.identity);
             }
 
         }
 
         void Update()
         {
-            if (active == true)
+             if (active == true)
                 Debug.Log(active);
             {
                 time += Time.deltaTime;
-                
+
                 if (time >= 10f)
                 {
                     enemyShoot.speed = 3;

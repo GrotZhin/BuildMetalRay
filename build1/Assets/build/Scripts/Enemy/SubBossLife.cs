1264117a98153d477b4d3a9be88b7104a76cb1e3
Reg-Meme <michaeljacksonnothenoite@gmail.com>
2024-12-03 13:14:04 -0300

    ver 2.0.8
    subboss flash effect

--- build1/Assets/build/Scripts/Enemy/SubBossLife.cs
+++ build1/Assets/build/Scripts/Enemy/SubBossLife.cs
@@ -20,10 +20,17 @@ namespace MetalRay
         Scene scene;
        public GameObject model;
 
+        [SerializeField] private GameObject flashEffect ;
+
+        
+        bool flashonoff = true;
+        int flashstop ;
+
         public void TakeDamage(int damage)
         {
             scene = SceneManager.GetActiveScene();    
             life -= damage;
+            FlashEffect();
             if (life <= 0)
             {
                 WinCondition.i +=1;
@@ -42,6 +49,39 @@ namespace MetalRay
             Instantiate(deathEffect, transform.position, Quaternion.identity);
             Instantiate(vfxhit, transform.position, Quaternion.identity);
             Destroy(gameObject);
+
+        }
+
+         void FlashEffect()
+        {
+
+           InvokeRepeating("Flash",0f,0.02f);
+
+        }
+
+          void Flash(){
+
+            if (flashonoff)
+            {
+            flashEffect.SetActive(false);
+            flashonoff= false;
+            flashstop ++;
+            }
+            else 
+            { 
+            flashEffect.SetActive(true);
+            flashonoff= true;
+            
+            }
+            if (flashstop >= 2f)
+            {
+
+            CancelInvoke("Flash");
+            flashEffect.SetActive(false);
+            flashstop = 0;
+
+            }
+        
 
         }
     }
